44a916fef891f2acedce22eef410922306f2af26
Kiattikhun <kiattikhun.wantamard@gmail.com>
2022-09-26 15:23:44 +0700

    edit text

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,13 @@ function App() {
 
     return (
         <main className={'grid place-content-center gap-2  overflow-hidden '}>
-            <h1 className={'text-center text-6xl text-red-300 text-transparent'}>Dra</h1>
+            <h1 className={'text-center text-6xl text-red-300 text-transparent'}>Drag !!!</h1>
 
             <div className={'border mt-4  relative border-red-400 h-[500px] w-[800px] overflow-hidden'}>
                 <div ref={dragRef} className={'w-[100px] absolute'}>
                     <div className={' bg-[#f1f1f1] cursor-pointer'}>
                         <img src={logo}/>
                     </div>
-
                 </div>
             </div>
         </main>
